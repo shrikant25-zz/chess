19cf4d690a2b2a48535e512186a24cc8c44fac1c
shrikant25 <shrikantwankhede24@gmail.com>
2021-11-20 17:03:45 +0530

    added movements for rook and knight

--- pieces.py
+++ pieces.py
@@ -1,6 +1,4 @@
-
 from squares import Square
-
 
 class Pieces:
     def __init__(self, name, row, column, color):        
@@ -36,15 +34,103 @@ class Rook(Pieces):
         super().__init__(name, row, column, color)
     
     def set_moves(self, board):
-        pass
+                                # upwards 
+        column = self.column
+        row = self.row - 1
+
+        while column >=0 and row >= 0:
+            if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                piece = board.get_piece_from_position(row, column)
+                if piece.color != self.color:
+                    piece.inpath = True
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+                break
+            else:
+                if not board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+            row -= 1
+                                #  right 
+        column = self.column + 1 
+        row = self.row
+
+        while column <=7 and row >= 0:
+            if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                piece = board.get_piece_from_position(row, column)
+                if piece.color != self.color:
+                    piece.inpath = True
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+                break
+            else:
+                if not board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+            column += 1
+                                #  left 
+        column = self.column - 1 
+        row = self.row
+
+        while column >=0 and row <= 7:
+            if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                piece = board.get_piece_from_position(row, column)
+                if piece.color != self.color:
+                    piece.inpath = True
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+                break
+            else:
+                if not board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+            column -= 1
+            row 
+                                # downwards 
+        column = self.column  
+        row = self.row + 1
+        
+        while column <=7 and row <= 7:
+            if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                piece = board.get_piece_from_position(row, column)
+                if piece.color != self.color:
+                    piece.inpath = True
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+                break
+            else:
+                if not board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
+            row += 1
+
 
 class Knight(Pieces):
     def __init__(self, name, row, column, color):
         super().__init__(name, row, column, color)
 
     def set_moves(self, board):
-       pass
-        
+        positions = [[ self.row - 1, self.column -2], # upward - left - 1 step
+        [self.row + 1 , self.column -2],  # downward - right - 1 step 
+        [self.row - 1 , self.column + 2], # upward - right - 1 step
+        [self.row + 1 , self.column + 2],# downward - right - 1 step
+        [self.row - 2 , self.column -1], # upward - left - 2 step,
+        [self.row - 2 , self.column + 1],  # upward - right - 2 step
+        [self.row + 2 , self.column  - 1],    # downward - left - 2 step
+        [self.row + 2 , self.column  + 1]# downward - right - 2 step
+        ]
+
+        for row, column in positions:
+            if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    piece = board.get_piece_from_position(row, column)
+                    if piece.color != self.color:
+                        piece.inpath = True
+                        square = board.get_square_from_position(row, column)
+                        square.possible_position = True
+            else:
+                if not board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
+                    square = board.get_square_from_position(row, column)
+                    square.possible_position = True
 
 class Bishop(Pieces):
     def __init__(self, name, row, column, color):
@@ -54,7 +140,7 @@ class Bishop(Pieces):
                                 # upwards - left - diagonal
         column = self.column - 1 
         row = self.row - 1
-        
+
         while column >=0 and row >= 0:
             if board.is_there_piece_on_position(row, column) and board.is_there_square_on_position(row, column):
                 piece = board.get_piece_from_position(row, column)
@@ -162,7 +248,6 @@ class Pawn(Pieces):
                     piece.inpath = True
                     square = board.get_square_from_position(self.row -1 , self.column + 1)
                     square.possible_position = True
-                    
         
         if self.color == 'black':
             if self.row == 1:
