1bf8f2e9c42133fc02fc7526f9cf40f6f2210f01
shrikant25 <shrikantwankhede24@gmail.com>
2021-12-02 22:01:38 +0530

    added code where computer can play against itself

--- main.py
+++ main.py
@@ -24,34 +24,53 @@ class Game:
         screen.update_board_display()    
         update_moves_of_all_pieces(screen.board, "white")
         run = True
+        result = False
         while run:
+            #if not result:
+                #result = self.do_stuff(screen, result)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
                 active_piece = screen.board.get_active_piece()
- 
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and active_piece == None:
                     screen.mouse_down()
-       
+    
                 if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and active_piece != None:
                     screen.mouse_up(active_piece)
                     update_moves_of_all_pieces(screen.board, screen.board.turn)
-                    
                     if not can_our_pieces_move(screen.board, screen.board.turn):
                         if king_under_attack(screen.board):
                             print(f"{active_piece.color} won")
                         else:
                             print("stale mate")
-                        run = False
-                    
                     if only_kings_remain(screen.board):
                         print("stale mate")
-                        run = False      
-       
                 screen.update_board_display()
-       
+ 
         pygame.quit()
+
+    def do_stuff(self, screen, result):
+        piece = generate_piece(screen.board)
+        screen.select_piece(piece.row, piece.column)
+        move = generate_move(piece)
+        screen.make_move(move[0], move[1], piece)
+
+
+        update_moves_of_all_pieces(screen.board, screen.board.turn)
+        if not can_our_pieces_move(screen.board, screen.board.turn):
+            if king_under_attack(screen.board):
+                print(f"{piece.color} won")
+                result = True
+            else:
+                print("stale mate")    
+                result = True
+        if only_kings_remain(screen.board):
+            print("stale mate")
+            result = True
+        screen.update_board_display()
+        return result
 
 if __name__ == '__main__':
     game = Game(8, 8, 712, 712, 700)
