107aa8a2c96060f3efc99fa5c57590567f3d65b6
shrikant25 <shrikantwankhede24@gmail.com>
2022-01-13 19:30:43 +0530

    added some code for testing purpose

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from colors import color
 from items import pieces
 from screen import Screen
 from cases import *
-
+dcnt = 0
 class Game:
     def __init__(self, rows, columns, width, height, board_dimensions):
         pygame.init()
@@ -18,28 +18,32 @@ class Game:
   
     def rungame(self): 
         pygame.display.set_caption("CHESS") 
-        self.window.fill(color['black']) 
+        self.window.fill(color['black'])
+        #for i in range(5):  
         screen = Screen(self.square_dimensions, self.rows, self.columns, self.window, self.borderline)
         screen.board.create_board(self.rows, self.columns)
         screen.update_board_display()    
         update_moves_of_all_pieces(screen.board)
         run = True
         result = False
-        depth = 1
-        print(f"depth 1 : {self.calculate(screen, depth)}")
-        #depth = 2
-        #print(f"depth 2 : {self.calculate(screen, depth)}")
-        #depth = 3
-        #print(f"depth 3 : {self.calculate(screen, depth)}")
-        
+            #depth = 1
+            #print(f"depth 1 : {self.calculate(screen, depth)}")
+            #depth = 2
+            #print(f"depth 2 : {self.calculate(screen, depth)}")
+            #depth = 3
+            #print(f"depth 3 : {self.calculate(screen, depth)}")
+            # global dcnt
+            #dcnt = 0
+            #print(f"depth {i} : {self.calculate(screen, i)}")
+            #print(dcnt)    
         while run:
             #if not result:
                 #result = self.do_stuff(screen, result)
-               
+              
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
-            """ 
+                   run = False
+             
                 active_piece = screen.board.get_active_piece()
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and active_piece == None:
                     screen.mouse_down()
@@ -63,15 +67,19 @@ class Game:
                 if result:
                     run = False
                     break  
-            """   
+                
         pygame.quit()
 
     def generate_moves(self, screen):
        
         moves = []
+        update_moves_of_all_pieces(screen.board)
         for piece in screen.board.pieces_list:
-            if piece.color == screen.board.turn:
-                for move in piece.possible_positions:      
+            if piece.color == screen.board.turn and piece.alive:
+                for mv in piece.possible_positions:                    
+                    move = mv[:]
+                    move.append(piece.row)
+                    move.append(piece.column)     
                     moves.append([piece, move])
         return moves
 
@@ -80,25 +88,42 @@ class Game:
        
         if depth == 0:
             return 1
-        pos = 0
-        update_moves_of_all_pieces(screen.board) 
+        
+        moves = []
         moves = self.generate_moves(screen)
+        pos = 0
         for piece, move in moves:
-            row = piece.row
-            column = piece.column
-            self.do_stuff(screen, piece, move)    
+            rs= self.do_stuff(screen, piece, move)
+            if not rs:
+                break    
             pos += self.calculate(screen, depth -1)
+            if screen.board.turn == 'white':
+                screen.board.turn = 'black' 
+            else:
+                screen.board.turn = 'white'
+            self.undo_move(piece, move)
             screen.update_board_display()
-            piece.row = row
-            piece.column = column
+            
         return pos
+
+    def undo_move(self, piece, move):
+        piece.row, piece.column = move[3], move[4] 
+        victim = move[2]
+        if victim:
+            victim.alive = True 
+            global dcnt
+            dcnt += 1
 
     def do_stuff(self, screen, piece, move):
         #piece = generate_piece(screen.board)
-        screen.select_piece(piece.row, piece.column)
+        if screen.select_piece(piece.row, piece.column):
         #move = generate_move(piece)
-        screen.make_move(move[0], move[1], piece)
-
+            active_piece = screen.board.get_active_piece()
+            #print(f"name {active_piece.name} row {active_piece.row} column {active_piece.column} mv {move}")
+            screen.make_move(move[0], move[1], active_piece, move[2])
+        else:
+            return False
+        return True
 
         """update_moves_of_all_pieces(screen.board, screen.board.turn)
         if not can_our_pieces_move(screen.board, screen.board.turn):
@@ -111,10 +136,9 @@ class Game:
         if only_kings_remain(screen.board):
             print("stale mate")
             #result = True"""
-        screen.update_board_display()
+        #screen.update_board_display()
         #return result
     
-  
 if __name__ == '__main__':
     game = Game(8, 8, 712, 712, 700)
     game.rungame()
