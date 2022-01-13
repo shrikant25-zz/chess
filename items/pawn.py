728289f5b8c7340378507c34036f81e9492006e5
shrikant25 <shrikantwankhede24@gmail.com>
2022-01-13 19:30:17 +0530

    changes the way enpassant works by fixing  a bug, now piece will not leave king vulnerable in a effort to kill oppostie by doing enpassant

--- items/pawn.py
+++ items/pawn.py
@@ -57,7 +57,7 @@ class Pawn(pieces.Pieces):
                 if piece.passed and piece.color != active_piece.color and piece.alive:
                     target_piece = piece
                         
-        if active_piece.piece_type == 'Pawn' and active_piece.can_do_enpassant:
+        if active_piece.piece_type == 'Pawn' and active_piece.can_do_enpassant and target_piece:
             if active_piece.color == 'white':
                 if not board.is_there_piece_on_position(target_piece.row - 1, target_piece.column):
                     active_piece.possible_positions.append([target_piece.row - 1, target_piece.column, target_piece])
@@ -84,14 +84,76 @@ class Pawn(pieces.Pieces):
                 adjacent_piece1 = board.get_piece_from_position(new_row, new_column + 1)
                 
                 if adjacent_piece1.piece_type == 'Pawn' and adjacent_piece1.color != active_piece.color:
-                    adjacent_piece1.can_do_enpassant = True
-                    active_piece.passed = True
-                    board.enpassant_possible = True
-            
+                    if self.is_safe_to_do_enpassant(board, adjacent_piece1, active_piece):
+                        adjacent_piece1.can_do_enpassant = True
+                        active_piece.passed = True
+                        board.enpassant_possible = True
+                    
             if board.is_there_piece_on_position(new_row, new_column - 1):
                 adjacent_piece1 = board.get_piece_from_position(new_row, new_column - 1)
                 
                 if adjacent_piece1.piece_type == 'Pawn' and adjacent_piece1.color != active_piece.color:
-                    adjacent_piece1.can_do_enpassant = True
-                    active_piece.passed = True
-                    board.enpassant_possible = True
+                    if self.is_safe_to_do_enpassant(board, adjacent_piece1, active_piece):
+                        adjacent_piece1.can_do_enpassant = True
+                        active_piece.passed = True
+                        board.enpassant_possible = True
+
+    def is_safe_to_do_enpassant(self, board, adjacent_piece1, active_piece):
+        side_vulnerable = False
+        king_is_dependent = True
+        
+        for piece in board.pieces_list:
+            if piece.color == adjacent_piece1.color and piece.piece_type == 'King' and piece.row == adjacent_piece1.row: 
+                if adjacent_piece1.column > piece.column: # it means king is on left and the piece is on right
+                    temp_column = adjacent_piece1.column - 1
+                    
+                    while temp_column > piece.column: # checking if is king is protected by adjacent pawn fron sideways attack
+                        if board.is_there_piece_on_position(adjacent_piece1.row,temp_column):
+                            if not (board.get_piece_from_position(adjacent_piece1.row,temp_column) == active_piece):    
+                                king_is_dependent = False
+                                break
+                        temp_column -= 1
+
+                    if king_is_dependent: # king is dependent on pawn to protect itself from right side attacks
+                        # even if the active piece slides in between the adjacent_piece and king it will be taken out by enpassant leaving the king wide open
+                        temp_column = adjacent_piece1.column + 1
+                                                # since king is on left it means adjacent piece is protecting king from right side attacks
+                        while temp_column <= 7: # checking if anyone can do sideways attack
+                            if board.is_there_piece_on_position(adjacent_piece1.row,temp_column):
+                                founded_piece = board.get_piece_from_position(adjacent_piece1.row,temp_column)
+                                
+                                if founded_piece.color != adjacent_piece1.color and (founded_piece.piece_type == 'Rook' or founded_piece.piece_type =='Queen'): # check if the piece is a sideways attacker
+                                    side_vulnerable = True # it means the side is vulnerable
+                                
+                                break # no need to loop anymore in this side
+
+                            temp_column += 1
+                    break # work is done as king and its vulnerabilites are discoverd
+
+                elif adjacent_piece1.column < piece.column: # it means the king is on right side and adjacent piece is on left side
+                    temp_column = adjacent_piece1.column + 1
+                    
+                    while temp_column < piece.column:
+                        if board.is_there_piece_on_position(adjacent_piece1.row,temp_column):
+                            if not (board.get_piece_from_position(adjacent_piece1.row,temp_column) == active_piece):    
+                                king_is_dependent = False
+                                break
+
+                        temp_column += 1
+
+                    if king_is_dependent:
+                        temp_column = adjacent_piece1.column - 1
+                        
+                        while temp_column >= 0:
+                            if board.is_there_piece_on_position(adjacent_piece1.row,temp_column):
+                                founded_piece = board.get_piece_from_position(adjacent_piece1.row,temp_column)
+                                
+                                if founded_piece.color != adjacent_piece1.color and (founded_piece.piece_type == 'Rook' or founded_piece.piece_type =='Queen'):
+                                    side_vulnerable = True
+                                
+                                break
+
+                            temp_column -= 1
+                    break
+        
+        return not side_vulnerable
